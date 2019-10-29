0fb1f70a3693a2d83a210958530e48e0cf07d19a
opzyra <opzyra@gmail.com>
2019-10-29 20:51:13 +0900

    Fix cross browsing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import './index.css';
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
